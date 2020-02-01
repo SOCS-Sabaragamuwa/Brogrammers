e2ec34559c5c2ee595675bbe63e25dc3476d0c52
shashimalcse <shashimalsenarath.17@cse.mrt.ac.lk>
2020-02-01 19:06:44 +0530

    route get  added

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 module.exports = function (app) {
     app.use("/api/users", require("./users"));
+    app.use("/api/routes", require("./vehicleroute"));
     app.use("/api/", (req, res) => {
         res.status(200).json({"message": "API up and running"})
     });
