023bd067311dd2e3b0550b287de5155af318f012
rumesh <rumesh.m.s16@gmail.com>
2020-02-02 01:27:12 +0530

    docker

--- routes/timetable/index.js
+++ routes/timetable/index.js
@@ -3,42 +3,42 @@ const Timetable = require("../../models/timetable");
 const pool = require("../../db");
 
 //Create user Route
-router.post("/", async (req, res) => {
-  let {
-    start_time,
-    end_time,
-    day,
-    officer_id,
-    vehicle_id,
-    route_id
-  } = req.body;
-  if (
-    start_time == null ||
-    end_time == null ||
-    day == null ||
-    officer_id == null ||
-    vehicle_id == null ||
-    route_id == null
-  ) {
-    res.status(400).send();
-    return;
-  }
-
-  let timetable = new Timetable();
-  let json_response = {};
-  try {
-    var results = null;
-    results = await timetable.createEntry(req.body);
-    json_response = {
-      ...results[0]
-    };
-    res.status(201).json(json_response);
-  } catch (e) {
-    if (e.code === 404) {
-      res.status(404).send();
-    }
-  }
-});
+// router.post("/", async (req, res) => {
+//   let {
+//     start_time,
+//     end_time,
+//     day,
+//     officer_id,
+//     vehicle_id,
+//     route_id
+//   } = req.body;
+//   if (
+//     start_time == null ||
+//     end_time == null ||
+//     day == null ||
+//     officer_id == null ||
+//     vehicle_id == null ||
+//     route_id == null
+//   ) {
+//     res.status(400).send();
+//     return;
+//   }
+//
+//   let timetable = new Timetable();
+//   let json_response = {};
+//   try {
+//     var results = null;
+//     results = await timetable.createEntry(req.body);
+//     json_response = {
+//       ...results[0]
+//     };
+//     res.status(201).json(json_response);
+//   } catch (e) {
+//     if (e.code === 404) {
+//       res.status(404).send();
+//     }
+//   }
+// });
 
 router.get("/:timetable_id", async (req, res) => {
   let timetable = new Timetable();
