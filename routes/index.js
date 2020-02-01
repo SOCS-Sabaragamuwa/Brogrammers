cfc9d8acf15446601e3fa481e28538f7e4c1e42a
rumesh <rumesh.m.s16@gmail.com>
2020-02-01 16:35:28 +0530

    get user details

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 module.exports = function (app) {
-    app.use("/reg_user", require("./user"));
-    //Root route-REMOVE this
+    app.use("/api/users", require("./users"));
     app.use("/api/", (req, res) => {
         res.status(200).json({"message": "API up and running"})
     });
